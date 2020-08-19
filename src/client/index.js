403b5704cfa1e292b1ae0580d344c9aea3fc8c7c
Miguel Dominguez Pérez <contacto@mdperez.es>
2020-08-19 13:50:05 +0200

    feat(config): adds Client library and config ports for dev and prod

--- src/client/index.js
+++ src/client/index.js
@@ -6,7 +6,12 @@ import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
+export {
+    checkForName,
+    handleSubmit
+}
 
-alert("I EXIST")
-console.log("CHANGE!!");
+// console.log(checkForName);
+
+// alert("I EXIST")
+// console.log("CHANGE!!");
