8e171c5c732130fc0320517b4e6319f380b53148
Miguel Dominguez Pérez <contacto@mdperez.es>
2020-08-19 17:19:59 +0200

    fix(linting): fixes some lint errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,35 +1,29 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!')
+});
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  res.sendFile('dist/index.html')
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
-
-console.log(`Your API key is ${process.env.API_KEY}`);
+  res.send(mockAPIResponse)
+});
