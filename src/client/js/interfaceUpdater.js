fb10c64e7f63fe30dd9e699e2c508261931fdc14
Miguel Dominguez Pérez <contacto@mdperez.es>
2020-08-20 14:26:09 +0200

    feat(styles): small design changes

--- src/client/js/interfaceUpdater.js
+++ src/client/js/interfaceUpdater.js
@@ -2,10 +2,10 @@
 function updateUI(data) {
   console.log(data);
   const result = `
-    <div id="agreement">Agreement: ${data.agreement}</div>
-    <div id="confidence">Confidence: ${data.confidence}</div>
-    <div id="irony">Irony: ${data.irony}</div>
-    <div id="subjectivity">Subjectiviy: ${data.subjectivity}</div>
+    <div id="agreement"><strong>Agreement:</strong> ${data.agreement}</div>
+    <div id="confidence"><strong>Confidence:</strong> ${data.confidence}</div>
+    <div id="irony"><strong>Irony:</strong> ${data.irony}</div>
+    <div id="subjectivity"><strong>Subjectiviy:</strong> ${data.subjectivity}</div>
   `;
   document.getElementById("results").innerHTML = result;
 }
